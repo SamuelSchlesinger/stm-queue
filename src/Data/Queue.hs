6b3cd46e0936a3e12c40f032d1ad08ff46cd5baa
Samuel Schlesinger <samuel@simspace.com>
2020-09-19 22:04:41 -0400

    Fixed the haddocks

--- src/Data/Queue.hs
+++ src/Data/Queue.hs
@@ -1,5 +1,14 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE BangPatterns #-}
+{- |
+Module: Data.Queue
+Description: A real-time, concurrent, and mutable queue
+Copyright: (c) Samuel Schlesinger 2020
+License: MIT
+Maintainer: sgschlesinger@gmail.com
+Stability: experimental
+Portability: POSIX, Windows
+-}
 module Data.Queue
 ( newQueue
 , peek
@@ -18,7 +27,7 @@ data Queue a = Queue
   !(TVar [a])
   !(TVar [a])
 
--- | Create a new, empty 'Queue'
+-- | Create a new, empty queue
 newQueue :: STM (Queue a)
 newQueue = Queue
   <$> newTVar []
@@ -41,16 +50,16 @@ queue (Queue top schedule bottom) =
       writeTVar bottom []
       writeTVar schedule rs
 
--- | Enqueue a single item onto the 'Queue'.
+-- | Enqueue a single item onto the queue.
 enqueue :: Queue a -> a -> STM ()
 enqueue q@(Queue _top _schedule bottom) a = do
   modifyTVar bottom (a :)
   queue q
 
--- | Dequeue a single item onto the 'Queue', 'retry'ing if there is nothing
+-- | Dequeue a single item onto the queue, 'retry'ing if there is nothing
 -- there. This is the motivating use case of this library, allowing a thread to
--- register its interest in the head of a 'Queue' and be woken up by the
--- runtime system to read from the top of that 'Queue' when an item has
+-- register its interest in the head of a queue and be woken up by the
+-- runtime system to read from the top of that queue when an item has
 -- been made available.
 dequeue :: Queue a -> STM a
 dequeue q@(Queue top _schedule _bottom) =
@@ -62,7 +71,7 @@ dequeue q@(Queue top _schedule _bottom) =
     [] -> retry
 
 -- | Try to 'dequeue' a single item. This function is offered to allow
--- users to easily port from the 'TQueue' offered in the 'stm' package,
+-- users to easily port from the 'TQueue' offered in the stm package,
 -- but is not the intended usage of the library.
 tryDequeue :: Queue a -> STM (Maybe a)
 tryDequeue q@(Queue top _schedule _bottom) =
@@ -73,15 +82,15 @@ tryDequeue q@(Queue top _schedule _bottom) =
       pure (Just x)
     [] -> pure Nothing
 
--- | Peek at the top of the 'Queue', returning the top element.
+-- | Peek at the top of the queue, returning the top element.
 peek :: Queue a -> STM a
 peek (Queue top _schedule _bottom) =
   readTVar top >>= \case
     x : xs -> pure x
     [] -> retry
 
--- | Try to 'peek' for the top item of the 'Queue'. This function is
--- offered to easily port from the 'TQueue' offered in the 'stm' package,
+-- | Try to 'peek' for the top item of the queue. This function is
+-- offered to easily port from the 'TQueue' offered in the stm package,
 -- but is not the intended usage of the library.
 tryPeek :: Queue a -> STM (Maybe a)
 tryPeek (Queue top _schedule _bottom) =
@@ -89,7 +98,7 @@ tryPeek (Queue top _schedule _bottom) =
     x : xs -> pure (Just x)
     [] -> pure Nothing  
 
--- | Efficiently read the entire contents of a 'Queue' into a list.
+-- | Efficiently read the entire contents of a queue into a list.
 flush :: Queue a -> STM [a]
 flush (Queue top schedule bottom) = do
   xs <- swapTVar top []
